26e1dc47b4fcc6689ae6d6ab951a69d0d6bddb09
Raul Navarro Cruz <esthalo17@gmail.com>
2019-12-16 20:24:50 +0100

    Update tests.py

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -59,8 +59,8 @@ class PostProcTestCase(APITestCase):
 
         expected_result = [
             { 'option': 'Partido 6', 'number': 6, 'votes': 170, 'postproc': 6 },
-            { 'option': 'Partido 1', 'number': 1, 'votes': 50, 'postproc': 2 },
             { 'option': 'Partido 5', 'number': 5, 'votes': 56, 'postproc': 2 },
+            { 'option': 'Partido 1', 'number': 1, 'votes': 50, 'postproc': 2 },
             { 'option': 'Partido 3', 'number': 3, 'votes': 34, 'postproc': 1 },
             { 'option': 'Partido 4', 'number': 4, 'votes': 25, 'postproc': 1 },
             { 'option': 'Partido 2', 'number': 2, 'votes': 10, 'postproc': 0 },
@@ -92,6 +92,35 @@ class PostProcTestCase(APITestCase):
             { 'option': 'Partido 2', 'number': 2, 'votes': 10, 'postproc': 0 },
             { 'option': 'Partido 3', 'number': 3, 'votes': 34, 'postproc': 0 },
             { 'option': 'Partido 4', 'number': 4, 'votes': 25, 'postproc': 0 },
+            { 'option': 'Partido 5', 'number': 5, 'votes': 56, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_dhondt3(self):
+        data = {
+            'type': 'DHONDT',
+            'seats': 2,
+            'options': [
+                { 'option': 'Partido 1', 'number': 1, 'votes': 2000 },
+                { 'option': 'Partido 2', 'number': 2, 'votes': 4000 },
+                { 'option': 'Partido 3', 'number': 3, 'votes': 1999 },
+                { 'option': 'Partido 4', 'number': 4, 'votes': 1998 },
+                { 'option': 'Partido 5', 'number': 5, 'votes': 56 },
+                { 'option': 'Partido 6', 'number': 6, 'votes': 170 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Partido 2', 'number': 1, 'votes': 4000, 'postproc': 2 },
+            { 'option': 'Partido 1', 'number': 2, 'votes': 2000, 'postproc': 0 },
+            { 'option': 'Partido 3', 'number': 3, 'votes': 1999, 'postproc': 0 },
+            { 'option': 'Partido 4', 'number': 4, 'votes': 1998, 'postproc': 0 },
+            { 'option': 'Partido 6', 'number': 6, 'votes': 170, 'postproc': 0 },
             { 'option': 'Partido 5', 'number': 5, 'votes': 56, 'postproc': 0 },
         ]
 
