bc0010c030f0359d623db53313bb12f4fc6cdb38
Jose Manuel <jmsx@hotmail.es>
2019-12-26 16:03:50 +0100

    Censo[fix]: Reparacion de Test
    *
    
    Se han reparado las urs diferenciando entre interfaz web y api para que no ocurran problemas con los test
    
    *

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.listaCensos, name='listaCensos'),
-    path('<int:voting_id>/', views.listaVotantes, name='listaVotantes'),
+    path('', views.CensusCreate.as_view(), name='census_create'),
+    path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
+    path('web/', views.listaCensos, name='list_census'),
+    path('web/<int:voting_id>/', views.listaVotantes, name='list_cesus_by_voting'),
 ]
