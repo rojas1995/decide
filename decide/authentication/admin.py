c7a3904edf74d7476adb84e1085dab77251479e8
Jose Manuel <jmsx@hotmail.es>
2019-12-30 16:00:45 +0100

    Censo[feat]: Añadida campos adicionales a "User", closes #35
    *
    Se han añadido los campos adicionales solucitados por el modulo booth. Estos son edad, sexo y municipio. son creados dentro Perfil.
    
    *

--- decide/authentication/admin.py
+++ decide/authentication/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from authentication.models import Perfil
 
-# Register your models here.
+admin.site.register(Perfil)
--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Perfil(models.Model):
+    SEXOS = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino')
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    sexo =  models.CharField(max_length=1, choices=SEXOS)
+    edad = models.PositiveIntegerField()
+    municipio = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
