3879f881e4112695186a06c8e7d857cb87441952
mcarmen <mcarmenarenas98@hotmail.es>
2019-12-30 15:10:19 +0100

    feat(init): register test

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SimpleTest(TestCase):
+def test_register(Self):
+
+    response = self.client.post('/register', { 'username': 'pedro', 'password': 'pedropedro'})
+    self.assertRedirects(response, '/login')
