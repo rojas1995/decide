eff3c79701e721164043e95e58672ac3e02f16f1
adddommon <anddommon@alum.us.es>
2020-01-02 15:53:59 +0100

    add test simple

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -72,3 +72,33 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
+    def test_simple1(self):
+        data = {
+            'type': 'SIMPLE',
+            'seats':40
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 5 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 0 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 3 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 5 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 2 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 2 },
+            { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 1 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 1 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 0 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
