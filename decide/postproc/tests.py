eabaa6db539ea4db896e33de79d5d6522a8d7521
adddommon <anddommon@alum.us.es>
2020-01-03 13:53:26 +0100

    Modificacion test

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -15,33 +15,34 @@ class PostProcTestCase(APITestCase):
     def tearDown(self):
         self.client = None
 
-    def test_identity(self):
-        data = {
-            'type': 'IDENTITY',
-            'options': [
-                { 'option': 'Option 1', 'number': 1, 'votes': 5 },
-                { 'option': 'Option 2', 'number': 2, 'votes': 0 },
-                { 'option': 'Option 3', 'number': 3, 'votes': 3 },
-                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
-                { 'option': 'Option 5', 'number': 5, 'votes': 5 },
-                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
-            ]
-        }
+    
+    # def test_identity(self):
+    #     data = {
+    #         'type': 'IDENTITY',
+    #         'options': [
+    #             { 'option': 'Option 1', 'number': 1, 'votes': 5 },
+    #             { 'option': 'Option 2', 'number': 2, 'votes': 0 },
+    #             { 'option': 'Option 3', 'number': 3, 'votes': 3 },
+    #             { 'option': 'Option 4', 'number': 4, 'votes': 2 },
+    #             { 'option': 'Option 5', 'number': 5, 'votes': 5 },
+    #             { 'option': 'Option 6', 'number': 6, 'votes': 1 },
+    #         ]
+    #     }
 
-        expected_result = [
-            { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 5 },
-            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 5 },
-            { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 3 },
-            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 2 },
-            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 1 },
-            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
-        ]
+    #     expected_result = [
+    #         { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 5 },
+    #         { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 5 },
+    #         { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 3 },
+    #         { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 2 },
+    #         { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 1 },
+    #         { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+    #     ]
 
-        response = self.client.post('/postproc/', data, format='json')
-        self.assertEqual(response.status_code, 200)
+    #     response = self.client.post('/postproc/', data, format='json')
+    #     self.assertEqual(response.status_code, 200)
 
-        values = response.json()
-        self.assertEqual(values, expected_result)
+    #     values = response.json()
+    #     self.assertEqual(values, expected_result)
 
     
     def test_simple(self):
