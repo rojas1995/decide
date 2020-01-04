39f27c1a580f01658858892485093c6bbc7459e2
adddommon <anddommon@alum.us.es>
2020-01-04 11:37:24 +0100

    ALgoritmo simple y test simple

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -42,3 +42,63 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
+
+    def test_simple(self):
+        data = {
+            'type': 'SIMPLE',
+            'seats':7,
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 5 }, #2.285
+                { 'option': 'Option 2', 'number': 2, 'votes': 0 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 3 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 5 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 2 },#0.188
+            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 2 },#0.188
+            { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 1 },#0.31
+            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 1 },#0.14
+            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 1 },#0.285
+            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_simple1(self):
+        data = {
+            'type': 'SIMPLE',
+            'seats':40,
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 5 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 0 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 3 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 5 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Option 1', 'number': 1, 'votes': 5, 'postproc': 13 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 13 },
+            { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 7 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 5 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 2 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import math
 
 
 class PostProcView(APIView):
@@ -15,10 +16,73 @@ class PostProcView(APIView):
 
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
+    def simple(self, options, seats):
+
+        out = []
+
+        for opt in options:
+
+            out.append({
+                **opt,
+                'postproc': 0,
+            })
+
+        out.sort(key=lambda x: -x['votes'])
+
+        ne = seats;
+        nv = 0;
+
+        for votes in out:
+            nv= nv+ votes['votes'];
+
+
+        valor_escano = nv/ne;
+
+        a = 0;        
+        
+        while ne > 0:
+            
+            if a < len(out):
+                
+                seats_ = math.trunc(out[a]['votes']/valor_escano)
+
+                out[a]['postproc'] = seats_;
+
+                ne = ne - seats_ ;
+                
+                a = a + 1
+           
+            else:
+
+                actual = 0;
+
+                i = 1;
+
+                while i < len(out):
+
+                    valorActual = out[actual]['votes']/valor_escano - out[actual]['postproc']
+
+                    valorComparate = out[i]['votes']/valor_escano - out[i]['postproc']
+
+                    if (valorActual >= valorComparate):
+
+                        i = i + 1;
+
+                    else:
+
+                        actual = i;
+
+                        i = i + 1;
+
+                out[actual]['postproc'] = out[actual]['postproc'] + 1;
+
+                ne = ne - 1;
+        
+        return Response(out)
 
     def post(self, request):
         """
-         * type: IDENTITY | EQUALITY | WEIGHT
+         * type: IDENTITY | EQUALITY | WEIGHT | SIMPLE
          * options: [
             {
              option: str,
@@ -34,5 +98,8 @@ class PostProcView(APIView):
 
         if t == 'IDENTITY':
             return self.identity(opts)
+        
+        elif t =='SIMPLE':
+            return self.simple(opts, s)
 
         return Response({})
