5fc216a42758e4786371fb64e778f25d5181bc5a
Yerehia <andangrei@alum.us.es>
2020-01-04 17:54:10 +0100

    fix(): data posting
    Vote's booth post works propertly

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import GetVoting, PageView, votinglist, VotingProcess
+from .views import GetVoting, PageView, votinglist, booth
 
 
 urlpatterns = [
-    path('<int:voting_id>/', VotingProcess.booth),
+    path('<int:voting_id>/', booth),
     path('', PageView.index),
     path('list/', votinglist),
     path('getvoting/', GetVoting.as_view())
--- decide/booth/views.py
+++ decide/booth/views.py
@@ -14,65 +14,61 @@ from django.shortcuts import render, redirect
 from Crypto.Random import random
 
 # TODO: check permissions and census
-class VotingProcess(TemplateView):
-    def booth(request, **kwargs):
-        if request.method == 'GET':
-            try:
-                voting_id = kwargs.get('voting_id')
-                voting = Voting.objects.get(pk = voting_id)
+def booth(request, **kwargs):
+    if request.method == 'GET':
+        try:
+            voting_id = kwargs.get('voting_id')
+            voting = Voting.objects.get(pk = voting_id)
 
-                return render(request, 'booth/booth.html', {'voting': voting})
-            except:
-                raise Http404
-        if request.method == 'POST':
-            try:
-                voting_id = kwargs.get('voting_id')
-                voting = Voting.objects.get(pk = voting_id)
-                option = int(request.POST['option'])
-                user_id = request.user.id
-                token = request.auth #Necesitamos que en el request este el auth
+            return render(request, 'booth/booth.html', {'voting': voting})
+        except:
+            raise Http404
+    if request.method == 'POST':
+        try:
+            voting_id = kwargs.get('voting_id')
+            voting = Voting.objects.get(pk = voting_id)
+            option = int(request.POST['option'])
+            user_id = request.user.id
+            token = str(Token.objects.get(user = request.user))#Necesitamos que en el request este el auth
 
-                bigpk = {
-                        'p': str(voting.pub_key.p),
-                        'g': str(voting.pub_key.g),
-                        'y': str(voting.pub_key.y),
-                    }
+            bigpk = {
+                    'p': str(voting.pub_key.p),
+                    'g': str(voting.pub_key.g),
+                    'y': str(voting.pub_key.y),
+                }
 
-                vote = encrypt(bigpk, option)
+            vote = encrypt(bigpk, option)
 
-                send_data(user_id, token, voting_id, vote)
-
-                return render(request, 'booth/success.html')
-            except:
-                raise Http404
-
-
-    def send_data(user, token, voting, vote):
-        data = {
-                'user': user,
-                'token': token,
-                'voting': voting,
-                'vote': {'a': str(vote[0]), 'b': str(vote[1])}
-            }
-
-        fdata = {
+            send_data(request, user_id, token, voting_id, vote)
             
-            'body': json.dumps(data),
-            'headers' : {
-                    'content-type': 'application/json',
-                    'Authorization': 'Token' + token
-                    }
-        }
-        r = requests.post(url = 'http://localhost:8000/gateway/store/', data = fdata)
-        print()
-        return r
-        
 
-    def encrypt(pk, M):
-        k = random.StrongRandom().randint(1, int(pk["p"]) - 1)
-        a = pow(int(pk["g"]), k, int(pk["p"]))
-        b = (pow(int(pk["y"]), k, int(pk["p"])) * M) % int(pk["p"])
-        return a, b
+            return render(request, 'booth/success.html')
+        except:
+            raise Http404
+
+
+def send_data(request, user, token, voting, vote):
+    data = json.dumps({
+            'voter': user,
+            'token': token,
+            'voting': voting,
+            'vote': {'a': str(vote[0]), 'b': str(vote[1])}
+        })
+
+    headers = {
+        'Content-type': 'application/json',
+        'Authorization': 'Token ' + token
+    }
+
+    r = requests.post(url = 'http://' + request.META['HTTP_HOST']+ '/gateway/store/', data = data, headers = headers)
+    return r
+    
+
+def encrypt(pk, M):
+    k = random.StrongRandom().randint(1, int(pk["p"]) - 1)
+    a = pow(int(pk["g"]), k, int(pk["p"]))
+    b = (pow(int(pk["y"]), k, int(pk["p"])) * M) % int(pk["p"])
+    return a, b
 
 def votinglist(request):
     user_id = request.user.id
@@ -87,7 +83,7 @@ def votinglist(request):
         return render(request, 'booth/votinglist.html', {'res':res})
     else:
         #If user is not log in, redirect to log in page.
-        return None
+        return PageView.login(request)
 
 class PageView(TemplateView):
 
