1b2e1f873d8dded7598c51e21e32b8f40c46e996
Fran <frabatnun97@gmail.com>
2020-01-05 19:30:36 +0100

    Census[chore]: modificado settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,6 +73,8 @@ MODULES = [
 ]
 
 BASEURL = 'https://censusfeature.herokuapp.com/'
+
+django_heroku.settings(locals())
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
