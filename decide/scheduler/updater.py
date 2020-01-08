49955efd7dd3e2c4afa08db7c9e85ac34d898152
Angel Delgado Luna <angeldelgadoluna@gmail.com>
2020-01-08 16:28:43 +0100

    feat(end): Terminado planificador para iniciar y detener votaciones
    Modulo planificador terminado

--- decide/scheduler/updater.py
+++ decide/scheduler/updater.py
@@ -7,8 +7,6 @@ from django.utils import timezone
 def start_votings():
     from voting.models import Voting
     now = timezone.now()
-    print(now.date())
-    print(now.time())
     
     findAllByNowDateSelected = Voting.objects.filter(start_date_selected__lte = now, pub_key=None)
     
@@ -34,7 +32,7 @@ def start():
 
     #Añadimos la tarea definiendo una funcion para que se ejecute en el intervalor que le marquemos.
     # Consultar: https://apscheduler.readthedocs.io/en/latest/userguide.html
-    scheduler.add_job(start_votings, 'interval', minutes=30)
+    scheduler.add_job(start_votings, 'interval', seconds=10)
     scheduler.add_job(end_votings, 'interval', minutes=30)
     # Le damos instrucciones al planificador para que comience
     scheduler.start()
