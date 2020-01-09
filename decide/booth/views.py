366fbd8566b4db6098908a91f6791c46e9c8b483
mcarmen <mcarmenarenas98@hotmail.es>
2020-01-09 23:54:52 +0100

    refactor(): añadida funcionalidad de profile

--- decide/booth/views.py
+++ decide/booth/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth import login as do_login
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from base import mods
-from booth.form import registerForm
+from booth.form import registerForm, profileForm
 from django.contrib import auth
 from census.models import Census
 from voting.models import Voting
@@ -157,8 +157,27 @@ class PageView(TemplateView):
         return render(request, 'booth/index.html')
     
     def profile(request):
-        print(request)
-        return render(request, 'booth/profile.html')
+        username = request.user.username
+        first_name = request.user.first_name
+        last_name = request.user.last_name
+        email = request.user.email
+        password = request.user.password
+
+        form = None
+        passwordError = 0
+        if request.method == "POST":
+            form = profileForm(request.POST, instance=request.user)
+            if form.is_valid():
+                if request.POST.get('password') == request.POST.get('confirm_password'):
+                    user = form.save()
+                    user.set_password(form.cleaned_data['password'])
+                    user.save()
+                    do_login(request, user)
+                    return redirect(request.META.get('HTTP_REFERER'))
+                else:
+                    passwordError = 1
+
+        return render(request, 'booth/profile.html', {'password': password,'passwordError': passwordError,'form': form, 'username': username, 'first_name': first_name, 'last_name': last_name, 'email': email})
 
 
 class GetVoting(APIView):
