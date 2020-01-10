4370827b53999dc61589ba80c32e12d1b52baf6e
Andrea <andreadommon12345@gmail.com>
2020-01-11 00:04:03 +0100

    Documentos heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -70,7 +70,9 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://git.heroku.com/decidepostproc.git'
+
+APIS = {}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -180,3 +182,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
