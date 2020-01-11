67ddf1e8e09270b6e072f9d29424536e71177d2b
rojas77 <josrojgra@alum.us.es>
2020-01-11 16:51:13 +0100

    un test más sobre paridad simple y uno unitario sobre paridad

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -2177,4 +2177,136 @@ class PostProcTestCase(APITestCase):
         values = response.json()
         self.assertEqual(values, expected_result)
 
+    def test_simple_conParidad3(self):
+
+        data = {
+            'type': 'SIMPLEP',
+            'seats':7,
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 5,  'candidatos': [
+                {'sexo':'mujer','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'mujer','id':'3'}
+                ,{'sexo':'hombre','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]}, 
+                { 'option': 'Option 2', 'number': 2, 'votes': 0,'candidatos': [
+                {'sexo':'mujer','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'mujer','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'mujer','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]},
+                { 'option': 'Option 3', 'number': 3, 'votes': 3, 'candidatos': [
+                {'sexo':'hombre','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'mujer','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]}, 
+                { 'option': 'Option 4', 'number': 4, 'votes': 2,'candidatos': [
+                {'sexo':'hombre','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ] },
+                { 'option': 'Option 5', 'number': 5, 'votes': 5 ,'candidatos': [
+                {'sexo':'hombre','id':'1'}
+                ,{'sexo':'hombre','id':'2'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'hombre','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]},
+                { 'option': 'Option 6', 'number': 6, 'votes': 1 ,'candidatos': [
+                {'sexo':'hombre','id':'1'}
+                ,{'sexo':'hombre','id':'2'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'hombre','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'hombre','id':'6'}
+                ]},
+        
+            ]
+        }
+
+        expected_result = {'message' : 'la diferencia del numero de hombres y mujeres es de más de un 60% - 40%'}
+
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_unitario_paridad5(self):
+        data = {
+            'type': 'PARIDAD',
+            'options': [
+                { 'option': 'Partido 1', 'number': 1, 'votes': 50 , 'postproc': 6, 'candidatos': [
+                 {'sexo':'mujer','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'mujer','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'mujer','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]},
+                { 'option': 'Partido 2', 'number': 2, 'votes': 10, 'postproc': 2, 'candidatos': [
+                 {'sexo':'hombre','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]},
+                { 'option': 'Partido 3', 'number': 3, 'votes': 34, 'postproc': 2, 'candidatos': [
+                 {'sexo':'hombre','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]},
+                { 'option': 'Partido 4', 'number': 4, 'votes': 25, 'postproc': 1, 'candidatos': [
+                 {'sexo':'hombre','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ] },
+                { 'option': 'Partido 5', 'number': 5, 'votes': 56, 'postproc': 1, 'candidatos': [
+                 {'sexo':'hombre','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]},
+                { 'option': 'Partido 6', 'number': 6, 'votes': 170, 'postproc': 0, 'candidatos': [
+                 {'sexo':'hombre','id':'1'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'hombre','id':'5'}
+                ,{'sexo':'mujer','id':'6'}
+                ]},
+            ]
+            
+        }
+
+        expected_result = {'message' : 'la diferencia del numero de hombres y mujeres es de más de un 60% - 40%'}
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+
         
