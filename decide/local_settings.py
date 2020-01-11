4372784c010234563318f83e42e44b2f5bd63195
Angel Delgado Luna <angeldelgadoluna@gmail.com>
2020-01-11 12:19:15 +0100

    fix(): Modificado cambio de nombre de base de datos en localsettings
    Borrado ficheros de migraciones antiguas y generado uno nuevo. Ademas, de cambiar el nombre a la BBDD del local settings

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -32,7 +32,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'USER': 'decide',
 	    'PASSWORD': 'decide', 
-        'NAME': 'postgres',
+        'NAME': 'decide',
         'HOST': '127.0.0.1',
         'PORT': '5432', 
     }
